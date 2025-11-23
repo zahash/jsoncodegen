83f8b741ff31d47686690cde489fbd92818b88eb
zahash <zahash.z@gmail.com>
2025-11-23 15:23:21 +0000

    wip type_graph

--- core/src/type_graph.rs
+++ core/src/type_graph.rs
@@ -117,105 +117,6 @@ impl GraphBuilder {
             }
         }
     }
-
-    // fn optimize_recursive_types(&mut self) {
-    //     #[derive(Debug)]
-    //     struct ResolvedObject {
-    //         type_ids: Vec<TypeId>,
-    //         code_fields: Vec<ObjectField>,
-    //     }
-
-    //     let mut resolved_objects: Vec<ResolvedObject> = vec![];
-    //     dbg!(&resolved_objects);
-
-    //     for (type_id, type_def) in &self.nodes {
-    //         dbg!(type_id, type_def);
-    //         if let TypeDef::Object(code_fields) = type_def {
-    //             let mut resolved = false;
-
-    //             dbg!(&resolved_objects);
-    //             for resolved_object in &mut resolved_objects {
-    //                 dbg!(&resolved_object);
-    //                 if let Some(resolved_code_fields) =
-    //                     self.resolve_code_fields(&resolved_object.code_fields, code_fields)
-    //                 {
-    //                     dbg!(&resolved_code_fields);
-    //                     *(&mut resolved_object.code_fields) = resolved_code_fields;
-    //                     resolved_object.type_ids.push(*type_id);
-    //                     dbg!(&resolved_object);
-    //                     resolved = true;
-    //                     break;
-    //                 }
-    //             }
-
-    //             dbg!(resolved);
-    //             if !resolved {
-    //                 let resolved_object = ResolvedObject {
-    //                     type_ids: vec![*type_id],
-    //                     code_fields: code_fields.clone(),
-    //                 };
-    //                 dbg!(&resolved_object);
-    //                 resolved_objects.push(resolved_object);
-    //                 dbg!(&resolved_objects);
-    //             }
-    //         }
-    //     }
-
-    //     dbg!(resolved_objects);
-    // }
-
-    // fn resolve_code_fields(
-    //     &self,
-    //     xs: &[ObjectField],
-    //     ys: &[ObjectField],
-    // ) -> Option<Vec<ObjectField>> {
-    //     dbg!(xs);
-    //     dbg!(ys);
-    //     if xs.len() != ys.len() {
-    //         return None;
-    //     }
-
-    //     dbg!(xs
-    //         .iter()
-    //         .zip(ys)
-    //         .map(|(x, y)| self.resolve_code_field(x, y))
-    //         .collect())
-    // }
-
-    // fn resolve_code_field(&self, a: &ObjectField, b: &ObjectField) -> Option<ObjectField> {
-    //     dbg!(a);
-    //     dbg!(b);
-    //     if a.name == b.name {
-    //         if a.type_id == b.type_id {
-    //             return dbg!(Some(a.clone()));
-    //         }
-
-    //         if matches!(self.nodes.get(&a.type_id), Some(TypeDef::Unknown)) {
-    //             return dbg!(Some(ObjectField {
-    //                 name: a.name.clone(),
-    //                 type_id: b.type_id.clone(),
-    //             }));
-    //         }
-
-    //         if matches!(self.nodes.get(&b.type_id), Some(TypeDef::Unknown)) {
-    //             return dbg!(Some(ObjectField {
-    //                 name: a.name.clone(),
-    //                 type_id: a.type_id.clone(),
-    //             }));
-    //         }
-    //     }
-
-    //     dbg!(None)
-    // }
-
-    // fn signature<'cf>(code_fields: impl IntoIterator<Item = &'cf CodeField>) -> Vec<&'cf str> {
-    //     let mut field_names: Vec<&str> = code_fields
-    //         .into_iter()
-    //         .map(|field| field.name.as_str())
-    //         .collect();
-    //     field_names.sort();
-    //     field_names
-    // }
 }
 
 #[derive(Default)]
@@ -242,13 +143,17 @@ impl TypeReducer {
     fn reduce_type_def(&mut self, type_def: TypeDef) -> TypeId {
         match type_def {
             TypeDef::Object(object_fields) => {
-                for (reduced_type_id, reduced_type_def) in &self.reduced_nodes {
+                for (reduced_type_id, reduced_type_def) in &mut self.reduced_nodes {
                     if let TypeDef::Object(reduced_object_fields) = reduced_type_def {
-                        if self.merge_object_fields(reduced_object_fields, &object_fields) {
+                        if let Some(merged_object_fields) =
+                            self.merge_object_fields(reduced_object_fields, &object_fields)
+                        {
+                            *reduced_object_fields = merged_object_fields;
                             return *reduced_type_id;
                         }
                     }
                 }
+
                 self.intern(TypeDef::Object(object_fields))
             }
             TypeDef::String
@@ -265,41 +170,64 @@ impl TypeReducer {
     }
 
     fn merge_object_fields(
-        &self,
-        target: &mut Vec<ObjectField>,
+        &mut self,
+        target: &[ObjectField],
         candidate: &[ObjectField],
-    ) -> bool {
+    ) -> Option<Vec<ObjectField>> {
         if target.len() != candidate.len() {
-            return false;
+            return None;
         }
 
-        let mut merged: Vec<ObjectField> = Vec::with_capacity(target.len());
-        for (target_field, candidate_field) in target.iter().zip(candidate.iter()) {
-            let Some(merged_field) = self.merge_object_field(target_field, candidate_field) else {
-                return false;
-            };
-            merged.push(merged_field);
-        }
-
-        *target = merged;
-        true
+        target
+            .iter()
+            .zip(candidate.iter())
+            .map(|(target_field, candidate_field)| {
+                self.merge_object_field(target_field, candidate_field)
+            })
+            // collecting Iterator<Item = Option<...>> as Option<Vec<...>>
+            // yeilds Some(Vec<...>) only if every iterated item is Some(...)
+            // else, None is yeilded
+            .collect()
     }
 
     fn merge_object_field(
-        &self,
+        &mut self,
         target: &ObjectField,
         candidate: &ObjectField,
     ) -> Option<ObjectField> {
         if target.name != candidate.name {
             return None;
-        } // names are same
+        } // names are same from here
 
         if target.type_id == candidate.type_id {
             return Some(target.clone());
-        } // types are different
+        } // types are different from here
 
-        todo!();
-        // let a =
+        let target_type_def = self.reduced_nodes.get(&target.type_id)?;
+
+        if let TypeDef::Unknown = target_type_def {
+            return Some(ObjectField {
+                name: candidate.name.clone(),
+                type_id: self.intern(TypeDef::Optional(candidate.type_id)),
+            });
+        }
+
+        if let TypeDef::Optional(target_inner_type_id) = target_type_def {
+            todo!()
+        }
+
+        let candidate_type_def = self.reduced_nodes.get(&candidate.type_id)?;
+
+        if let TypeDef::Unknown = candidate_type_def {
+            return Some(ObjectField {
+                name: target.name.clone(),
+                type_id: self.intern(TypeDef::Optional(target.type_id)),
+            });
+        }
+
+        if let TypeDef::Optional(candidate_inner_type_id) = candidate_type_def {
+            todo!()
+        }
 
         None
     }
@@ -341,107 +269,6 @@ impl TypeReducer {
         }
     }
 }
-
-/*
-impl CodeGraph {
-    fn handle_recursive_types(&mut self) {
-        fn signature(code_fields: &[CodeField]) -> Vec<&str> {
-            let mut field_names: Vec<&str> = code_fields
-                .iter()
-                .map(|field| field.name.as_str())
-                .collect();
-            field_names.sort();
-            field_names
-        }
-
-        fn clump<'o, 'cf>(
-            objects: &'o [(TypeId, &'cf [CodeField])],
-        ) -> Vec<Vec<&'o (TypeId, &'cf [CodeField])>> {
-            fn can_clump(fields_a: &[CodeField], fields_b: &[CodeField]) -> bool {
-                match (fields_a, fields_b) {
-                    (
-                        [fields_a_first, fields_a_rest @ ..],
-                        [fields_b_first, fields_b_rest @ ..],
-                    ) => {}
-                    _ => {}
-                }
-
-                todo!()
-            }
-
-            todo!()
-
-            // match objects {
-            //     [first_obj, rest @ ..] => {
-            //         let mut clumped = Cow::Borrowed(first_obj);
-            //         for next_obj in rest {
-            //             let (_, first_obj_code_fields) = first_obj;
-            //             let (_, next_obj_code_fields) = next_obj;
-            //             if can_clump(first_obj_code_fields, next_obj_code_fields) {
-            //                 clumped.push(next_obj);
-            //             }
-            //         }
-
-            //         let mut next_clump = clump(rest);
-            //         next_clump.push(clumped);
-            //         next_clump
-            //     }
-            //     _ => vec![],
-            // }
-        }
-
-        let object_groups = {
-            let mut type_groups: BTreeMap<Vec<&str>, Vec<(TypeId, &[CodeField])>> = BTreeMap::new();
-            for (type_id, type_def) in &self.nodes {
-                if let TypeDef::Object(code_fields) = type_def {
-                    let signature = signature(code_fields);
-                    type_groups
-                        .entry(signature)
-                        .or_default()
-                        .push((*type_id, code_fields));
-                }
-            }
-            type_groups
-        };
-
-        for objects in object_groups.values() {
-            let clumped = clump(objects);
-            todo!()
-        }
-    }
-
-    fn apply_remapings(&mut self, map: BTreeMap<TypeId, TypeId>) {
-        if map.is_empty() {
-            return;
-        }
-
-        for type_def in self.nodes.values_mut() {
-            match type_def {
-                TypeDef::Object(code_fields) => {
-                    for field in code_fields {
-                        if let Some(target_type_id) = map.get(&field.type_id) {
-                            field.type_id = *target_type_id;
-                        }
-                    }
-                }
-                TypeDef::Union(type_ids) => {
-                    for type_id in type_ids {
-                        if let Some(target_type_id) = map.get(type_id) {
-                            *type_id = *target_type_id;
-                        }
-                    }
-                }
-                TypeDef::Array(type_id) | TypeDef::Optional(type_id) => {
-                    if let Some(target_type_id) = map.get(type_id) {
-                        *type_id = *target_type_id;
-                    }
-                }
-                _ => { /* no-op */ }
-            }
-        }
-    }
-}
-*/
 
 #[cfg(test)]
 mod tests {

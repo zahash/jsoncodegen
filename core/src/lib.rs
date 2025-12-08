e01df46789361d135891cf763022cb63179135aa
zahash <zahash.z@gmail.com>
2025-12-08 04:36:09 +0000

    add todo comment

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -5,3 +5,5 @@ pub mod type_graph;
 // TODO: use indexmap to preserve insertion order or not? maybe keep them sorted
 // TODO: write code to parse the display string of schema and type_graph into the respective types.
 //          ideally keep it under #[cfg(test)]
+// TODO: differentiate between Null and Unknown types.
+//          right now both are combined under Unknown but behave like Null

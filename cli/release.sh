22dfd838ff74c7e6e622ac20de47d29dd0bbd1ba
zahash <zahash.z@gmail.com>
2025-12-10 00:51:21 +0530

    update release.sh to include windows gnu

--- cli/release.sh
+++ cli/release.sh
@@ -6,9 +6,10 @@ if [ -z "$1" ]; then
     echo "Usage: $0 <target-triple>"
     echo ""
     echo "Example target triples:"
-    echo "  Linux:   x86_64-unknown-linux-gnu, aarch64-unknown-linux-gnu, i686-unknown-linux-gnu"
-    echo "  Windows: x86_64-pc-windows-msvc, i686-pc-windows-msvc, aarch64-pc-windows-msvc"
-    echo "  macOS:   aarch64-apple-darwin, x86_64-apple-darwin"
+    echo "  Linux:          x86_64-unknown-linux-gnu, aarch64-unknown-linux-gnu, i686-unknown-linux-gnu"
+    echo "  Windows (msvc): x86_64-pc-windows-msvc, aarch64-pc-windows-msvc, i686-pc-windows-msvc"
+    echo "  Windows (gnu):  x86_64-pc-windows-gnu, aarch64-pc-windows-gnu, i686-pc-windows-gnu"
+    echo "  macOS:          aarch64-apple-darwin, x86_64-apple-darwin"
     exit 1
 fi
 
